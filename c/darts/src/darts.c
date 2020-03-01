e30f8c245ef21d550ea870653a671099fc13d32a
lcmrvk <lcmo@protonmail.com>
2020-03-01 10:42:12 +0100

    Finished 'darts' exercise

--- c/darts/src/darts.c
+++ c/darts/src/darts.c
@@ -1,1 +1,54 @@
+#include <stdio.h>
 #include "darts.h"
+
+#define COMPILE_NO_EXERCISM_TEST 0
+#define DEBUG 0
+
+#if COMPILE_NO_EXERCISM_TEST == 1
+int main(void)
+{
+
+	VERIFY_COORDS(9, 9);
+	VERIFY_COORDS(0, 10);
+	VERIFY_COORDS(-5, 0);
+	VERIFY_COORDS(0, -1);
+	VERIFY_COORDS(0, 0);
+	VERIFY_COORDS(-0.1, -0.1);
+	VERIFY_COORDS(0.7, 0.7);
+	VERIFY_COORDS(0.8, -0.8);
+	VERIFY_COORDS(-3.5, 3.5);
+	VERIFY_COORDS(-3.6, -3.6);
+	VERIFY_COORDS(-7.0, 7.0);
+	VERIFY_COORDS(7.1, -7.1);
+	VERIFY_COORDS(-0.5, -4.0);
+
+	return 0;
+}
+#endif // COMPILE_NO_EXERCISM_TEST
+
+int
+score(coordinate_t hit)
+{
+	coordinate_t origin = {0, 0};
+	double distance = EUCLIDIAN_DISTANCE(origin, hit);
+
+	#if DEBUG == 1
+		printf("distance is: %f | ", distance);
+	#endif // DEBUG
+
+	if (distance > OUTER_CIRCLE) {
+		return MISS_POINTS;
+	}
+	else if (distance > MIDDLE_CIRCLE) {
+		return OUTER_CIRCLE_POINTS;
+	}
+	else if (distance > INNER_CIRCLE) {
+		return MIDDLE_CIRCLE_POINTS;
+	}
+	else {
+		return INNER_CIRCLE_POINTS;
+	}
+
+	return DISTANCE_ERROR;
+
+}
--- c/darts/src/darts.h
+++ c/darts/src/darts.h
@@ -1,4 +1,33 @@
 #ifndef DARTS_H
 #define DARTS_H
 
+#define MISS_POINTS 0
+#define OUTER_CIRCLE 10
+#define OUTER_CIRCLE_POINTS 1
+#define MIDDLE_CIRCLE 5
+#define MIDDLE_CIRCLE_POINTS 5
+#define INNER_CIRCLE 1
+#define INNER_CIRCLE_POINTS 10
+#define DISTANCE_ERROR -1
+
+typedef struct
+{
+	double x;
+	double y;	
+} coordinate_t;
+
+int score(coordinate_t hit);
+
+#ifndef _MATH_H
+ #include <math.h>
 #endif
+
+#define COORD_DIFF(coordinate_t_1, coordinate_t_2, c) (pow((coordinate_t_1.c) - (coordinate_t_2.c), 2))
+#define EUCLIDIAN_DISTANCE(coordinate_t_1, coordinate_t_2) \
+			(sqrt( COORD_DIFF(coordinate_t_1, coordinate_t_2, x)  \
+				+ COORD_DIFF(coordinate_t_1, coordinate_t_2, y)))
+
+#define VERIFY_COORDS(x, y) (printf( "(" #x "," #y ")"  ": %d\n", score( (coordinate_t) {x, y}) ))
+
+
+#endif // DARTS_H

3140dd4fcbab90ed9737f0e6602d9e7186c07c29
lcmrvk <lcmo@protonmail.com>
2020-03-03 23:31:49 +0100

    Finished 'resistor-color' exercise

--- c/resistor-color/src/resistor_color.c
+++ c/resistor-color/src/resistor_color.c
@@ -1,1 +1,18 @@
 #include "resistor_color.h"
+
+static const resistor_band_t color_values[] = { 
+	BLACK, BROWN, RED, ORANGE, YELLOW,
+	GREEN, BLUE, VIOLET, GREY, WHITE, COLOR_ERROR
+};
+
+resistor_band_t color_code(resistor_band_t color) {
+
+	if (color >= BLACK && color <= WHITE)
+		return color_values[color];
+
+	return COLOR_ERROR;
+}
+
+const resistor_band_t * colors() {
+	return color_values;
+}
--- c/resistor-color/src/resistor_color.h
+++ c/resistor-color/src/resistor_color.h
@@ -1,4 +1,14 @@
 #ifndef RESISTOR_COLOR_H
 #define RESISTOR_COLOR_H
 
+
+typedef enum {
+	BLACK, BROWN, RED, ORANGE, YELLOW,
+	GREEN, BLUE, VIOLET, GREY, WHITE, COLOR_ERROR
+} resistor_band_t;
+
+
+resistor_band_t color_code(resistor_band_t color);
+const resistor_band_t * colors();
+
 #endif

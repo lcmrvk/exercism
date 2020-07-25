dcbe9a0cbe63830d2ec122f64c6bc9401b0e3625
lcmrvk <lcmo@protonmail.com>
2020-07-26 00:11:49 +0200

    Finished 'resistor-color-trio' exercise

--- c/resistor-color-trio/src/resistor_color_trio.c
+++ c/resistor-color-trio/src/resistor_color_trio.c
@@ -1,1 +1,28 @@
+#include <stdint.h>
+#include <math.h>
+
 #include "resistor_color_trio.h"
+
+
+#if COMPILE_NO_EXERCISM_TEST == 1
+int main(void)
+{
+	return 0;
+}
+#endif // COMPILE_NO_EXERCISM_TEST
+
+
+resistor_value_t color_code(resistor_band_t *band) {
+
+	resistor_value_t result;
+
+	result.value = (10 * band[0] + band[1]) * pow(10, band[2]);
+	if (result.value < 1000)  {
+		result.unit = OHMS;
+	} else {
+		result.value /= 1000;
+		result.unit = KILOOHMS;
+	}
+
+	return result;
+}
--- c/resistor-color-trio/src/resistor_color_trio.h
+++ c/resistor-color-trio/src/resistor_color_trio.h
@@ -1,4 +1,19 @@
 #ifndef RESISTOR_COLOR_TRIO_H
 #define RESISTOR_COLOR_TRIO_H
 
+#define OHMS 0
+#define KILOOHMS 1
+
+typedef enum {
+  BLACK, BROWN, RED, ORANGE, YELLOW,
+  GREEN, BLUE, VIOLET, GREY, WHITE, COLOR_ERROR
+} resistor_band_t;
+
+typedef struct {
+ 	uint32_t value;
+ 	uint8_t unit;
+} resistor_value_t;
+
+resistor_value_t color_code(resistor_band_t *band);
+
 #endif
